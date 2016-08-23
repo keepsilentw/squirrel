2369a01c3f9f4c8ecf2f7d25884e20ed30e3ad50
silas <369694143@qq.com>
2016-08-23 14:19:17 +0800

    gather more

--- fe/js/webpack.config.js
+++ fe/js/webpack.config.js
@@ -1,82 +1,63 @@
+'use strict';
+
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const CleanPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
-const vendor = [
-  './node_modules/angular/angular.js',
-  './node_modules/angular-animate/angular-animate.js',
-  './node_modules/angular-chart.js/dist/angular-chart.js',
-  './node_modules/angular-file-upload/dist/angular-file-upload.js',
-  './node_modules/angular-h-sweetalert/dist/ngSweetAlert.js',
-  './node_modules/angular-i18n/angular-locale_zh-cn.js',
-  './node_modules/angular-loading-bar/build/loading-bar.js',
-  './node_modules/angular-resource/angular-resource.js',
-  './node_modules/angular-ui-bootstrap',
-  './node_modules/angular-ui-router/release/angular-ui-router.js',
-  './node_modules/angularjs-toaster/toaster.js'
-];
+const TARGET = process.env.npm_lifecycle_event;
+const DIST = path.join(__dirname, 'dist');
 
 const common = {
-  entry: {vendor: vendor, app: './src/app.js'},
-  output: {filename: '[name].[hash].js'},
+  entry: [path.join(__dirname, '/src/app.js')],
+  output: {path: DIST, filename: 'app.[hash].js'},
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel', exclude: [/node_modules/, /base\.js/]},
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap')},
-      {test: /\.scss$/, loader: 'style!css!sass'},
-      {test: /\.(png|jpg)$/, loader: 'url?limit=32768'},
-      {test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]', exclude: /index\.html/},
-      {test: /\.json$/, loader: 'json'},
-      {test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery'},
-      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+      {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap')},
+      {test: /\.(png|jpg)$/, loader: 'url?limit=32768'}
     ]
   },
-  resolve: {alias: {'jqueryDownload': 'jquery-file-download/src/Scripts/jquery.fileDownload.js'}},
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new CopyWebpackPlugin([{from: './src/base.js'}, {from: './src/base.js.etpl'}]),
     new ExtractTextPlugin('styles.[chunkhash].css', {allChunks: true}),
     new HtmlWebpackPlugin({template: path.resolve('src', 'index.html'), inject: 'body'}),
-    new webpack.ProvidePlugin({'$': 'jquery', 'jQuery': 'jquery'})
   ]
 }
 
-const TARGET = process.env.npm_lifecycle_event;
-
-if (TARGET === 'dev') {
+if (TARGET === 'start') {
   module.exports = merge(common, {
-    devtool: 'source-map',
+    entry: ['webpack/hot/dev-server', 'webpack/hot/only-dev-server'],
+    devtool: 'eval-source-map',
     devServer: {
       contentBase: './src',
       historyApiFallback: true,
+      hot: true,
       inline: true,
       progress: true,
       stats: 'errors-only',
       host: 'localhost',
-      port: 8000
+      port: 3000
     },
-    plugins: [new OpenBrowserPlugin({url: 'http://localhost:8000'})]
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new OpenBrowserPlugin({url: 'http://localhost:3000'})
+    ]
   })
 }
 
-const DIST = path.join(__dirname, 'dist');
-
-if (TARGET === 'dep') {
+if (TARGET === 'build') {
   module.exports = merge(common, {
-    output: {path: DIST},
     plugins: [
       new CleanPlugin([DIST]),
       new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}, output: {comments: false}})
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {warnings: false},
+        output: {comments: false}
+      })
     ]
   })
 }
